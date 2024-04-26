3f2890e1856e331a1b11df1f9744b59fde89c01c
砰砰枪 <18860976892@163.com>
2024-04-27 01:57:59 +0800

    fix:取消Next.js强制缓存

--- app/api/blog/search/route.ts
+++ app/api/blog/search/route.ts
@@ -1,12 +1,26 @@
-import { getPublishedBlogs } from "@/services/blogs";
+import prisma from "@/lib/prisma";
+// import { getPublishedBlogs } from "@/services/blogs";
 
 // 查询所有博客
 export async function GET() {
-  const { blogs } = await getPublishedBlogs();
+  const blogs = await prisma.blog.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+    include: {
+      tags: true,
+    },
+    where: {
+      published: true,
+    },
+  });
 
   console.log("blogs--", blogs);
 
   return Response.json({
-    blogs,
+    blogs: blogs.map((item) => {
+      const { body, ...rest } = item;
+      return rest;
+    }),
   });
 }
